a1a9e1436d13a4e3f88ee78aefa64d1e4b452df3
Murilo <murilo.ti32@outlook.com>
2023-10-24 17:52:30 -0300

    ini

---
+++ Variaveis e operacoes/aula.js
@@ -0,0 +1,19 @@
+//Formas de declarar variavel
+let variavel = 'valor';
+
+variavel = 10
+
+// const pi = 3.14;
+
+
+//Operadores aritmeticos
+
+variavel = 10 + 10
+console.log(variavel)
+variavel = 10 - 10
+console.log(variavel)
+variavel = 10 / 10
+console.log(variavel)
+variavel = 10 * 10
+
+console.log(variavel)
---
+++ Variaveis e operacoes/primeiro_desafio.js
@@ -0,0 +1,16 @@
+/*
+Faça um programa para calcular o valor de uma viagem.
+
+Você terá 3 variaveis. Sendo elas:
+    1- Preço de combustivel
+    2 - Gasto médio de combustivel do carro por KM
+    3 - Distância em KM da viagem
+
+Imprima no console o valor que será gasto de combustivel para realizar esta viagem
+*/
+
+const pretoCombustivel = 5.79;
+const kmPorLitros      = 10;
+const distancia        = 100;
+
+
